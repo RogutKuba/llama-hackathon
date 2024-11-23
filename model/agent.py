599cf188edc1824054cd3a0a322e7ee568bbe73f
My Name <sungjinhong@cohere.com>
2024-11-23 15:47:28 -0500

    vision web agent

---
+++ model/agent.py
@@ -0,0 +1,87 @@
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from IPython import display
+from playwright.async_api import async_playwright
+import asyncio
+import base64
+
+from model.annotate_page import mark_page
+
+async def annotate(state):
+    marked_page = await mark_page.with_retry().ainvoke(state["page"])
+    return {**state, **marked_page}
+
+
+def format_descriptions(state):
+    labels = []
+    for i, bbox in enumerate(state["bboxes"]):
+        text = bbox.get("ariaLabel") or ""
+        if not text.strip():
+            text = bbox["text"]
+        el_type = bbox.get("type")
+        labels.append(f'{i} (<{el_type}/>): "{text}"')
+    bbox_descriptions = "\nValid Bounding Boxes:\n" + "\n".join(labels)
+    return {**state, "bbox_descriptions": bbox_descriptions}
+
+
+def parse(text: str) -> dict:
+    action_prefix = "Action: "
+    if not text.strip().split("\n")[-1].startswith(action_prefix):
+        return {"action": "retry", "args": f"Could not parse LLM Output: {text}"}
+    action_block = text.strip().split("\n")[-1]
+
+    action_str = action_block[len(action_prefix) :]
+    split_output = action_str.split(" ", 1)
+    if len(split_output) == 1:
+        action, action_input = split_output[0], None
+    else:
+        action, action_input = split_output
+    action = action.strip()
+    if action_input is not None:
+        action_input = [
+            inp.strip().strip("[]") for inp in action_input.strip().split(";")
+        ]
+    return {"action": action, "args": action_input}
+
+
+
+
+async def open_browser(url: str = "https://www.google.com"):
+    playwright = await async_playwright().start()
+    browser = await playwright.chromium.launch(headless=False)
+    page = await browser.new_page()
+    await page.goto(url)
+
+    print("Browser is open and page is returned. Press Ctrl+C to exit when done.")
+    return page
+
+async def call_agent(graph, question: str, page, max_steps: int = 150):
+    event_stream = graph.astream(
+        {
+            "page": page,
+            "input": question,
+            "scratchpad": [],
+        },
+        {
+            "recursion_limit": max_steps,
+        },
+    )
+    final_answer = None
+    steps = []
+    async for event in event_stream:
+        # We'll display an event stream here
+        if "agent" not in event:
+            continue
+        pred = event["agent"].get("prediction") or {}
+        action = pred.get("action")
+        action_input = pred.get("args")
+        display.clear_output(wait=False)
+        steps.append(f"{len(steps) + 1}. {action}: {action_input}")
+        print("\n".join(steps))
+        display.display(display.Image(base64.b64decode(event["agent"]["img"])))
+        if "ANSWER" in action:
+            final_answer = action_input[0]
+            break
+    return final_answer
---
+++ model/main.py
@@ -0,0 +1,71 @@
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+import re
+from model.agent import annotate
+from model.schema import AgentState, SystemMessage
+from model.build_graph import update_scratchpad
+from langchain_core.runnables import RunnableLambda
+
+from langgraph.graph import END, START, StateGraph
+from model.tools import click, go_back, scroll, to_google, type_text, wait, TOOLS
+from model.agent import annotate, format_descriptions, parse, open_browser, call_agent
+from model.build_graph import update_scratchpad, select_tool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def initialize_agent():
+    prompt = hub.pull("wfh/web-voyager")
+    # print(prompt.messages[0].prompt[0].template)
+    llm = ChatOpenAI(model="gpt-4o", max_tokens=4096)
+    agent = annotate | RunnablePassthrough.assign(
+        prediction=format_descriptions | prompt | llm | StrOutputParser() | parse
+    )
+    return agent
+
+def initialize_graph():
+    agent = initialize_agent()
+    graph_builder = StateGraph(AgentState)
+
+    graph_builder.add_node("agent", agent)
+    graph_builder.add_edge(START, "agent")
+
+    graph_builder.add_node("update_scratchpad", update_scratchpad)
+    graph_builder.add_edge("update_scratchpad", "agent")
+
+    # Adding tools as nodes
+    for node_name, tool in TOOLS.items():
+        graph_builder.add_node(
+            node_name,
+            RunnableLambda(tool) | (lambda observation: {"observation": observation}),
+        )
+        graph_builder.add_edge(node_name, "update_scratchpad")  # Connect to update_scratchpad
+
+    graph_builder.add_conditional_edges("agent", select_tool)
+
+    return graph_builder.compile()
+
+async def fetch_page_content_and_query(user_question: str, graph: StateGraph, page):
+    response = await call_agent(graph, user_question, page)
+    
+    return response
+
+async def main(test_question):
+    page = await open_browser()
+    print("PAGE OPENED")
+    graph = initialize_graph()
+    # Fetch the content and query
+    result = await fetch_page_content_and_query(test_question, graph, page)
+    print("Agent Response:", result)
+    # close the browser:
+    await page.context.close()
+
+if __name__ == "__main__":
+    import asyncio
+    test_question = "What are some of the latest news from CBC?"
+    asyncio.run(main(test_question))
+    # python -m model.main
+
